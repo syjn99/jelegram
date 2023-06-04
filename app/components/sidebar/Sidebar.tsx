5e09116bba67ce83b91692f01a66653b7ef4e5ce
syjn99 <syjn99@gmail.com>
2023-06-04 21:25:04 +0900

    feat: Add MobileFooter

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { DesktopSidebar } from "./DesktopSidebar";
+import { MobileFooter } from "./MobileFooter";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -8,6 +9,7 @@ async function Sidebar({ children }: SidebarProps) {
   return (
     <div className="h-full">
       <DesktopSidebar />
+      <MobileFooter />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   );
